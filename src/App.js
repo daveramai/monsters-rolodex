78facc3014197eb676c3054f7abe020dcd2ec075
Divindra Ramai <daveramai@hotmail.com>
2020-06-02 00:23:27 -0400

    Converted App function to class and inserted button - baseline

--- src/App.js
+++ src/App.js
@@ -2,25 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      message: 'Hello Dave'
+    }
+  } //constructor
+
+  ChangeState = () => {
+    this.setState({message: 'Look Up'})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.message}</p>
+          <button onClick={this.ChangeState}>Change State</button>
+        </header>
+      </div>
+    );
+  } //render
+} //class
+
+
+  
+
 
 export default App;
