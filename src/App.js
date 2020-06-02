722ef77a57d889aaa16a82674add10c63210ec7d
Divindra Ramai <daveramai@hotmail.com>
2020-06-02 02:41:02 -0400

    Search field added to filter monster cards dynamically

--- src/App.js
+++ src/App.js
@@ -8,32 +8,29 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: '1'
-        },
-        {
-          name: 'Dracula',
-          id: '2'
-        },
-        {
-          name: 'Zombie',
-          id: '3'
-        }
-      ]
+      monsters: [],
+      searchField: ''
     };
   } //constructor
 
-  ChangeState = () => {
-    this.setState({message: 'Look Up'})
+  onChange = (e) => {
+    console.log(e.target.value);
+    this.setState({searchField: e.target.value});
   }
 
   render() {
+    // destructure state into reusable array variables
+    const { monsters, searchField } = this.state;
+    // filter new monster array based on search
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    // return
     return (
       <div className="App">
+        {/* Searchbox to filter by monster */}
+        <input onChange={this.onChange} type="search" placeholder="search monster" />
         {/* CardList component - responsible for listing all the cards */}
-        <CardList monsters={this.state.monsters}/>
+        <CardList monsters={filteredMonsters}/>
       </div>
     ); //return
   } //render
--- src/component/card/card.component.jsx
+++ src/component/card/card.component.jsx
@@ -4,6 +4,8 @@ import './card.styles.css';
 // rafc + TAB for template
 export const Card = (props) => (
     <div className="card-container">
-        <h1> {props.monster.name} </h1>    
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set1&size=180x180`} />
+        <h2> {props.monster.name} </h2>
+        <p> {props.monster.email} </p>    
     </div>
 )
